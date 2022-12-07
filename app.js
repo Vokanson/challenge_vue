5c5ba5ff9d24364f9bd0cfac0d8dff7efdc8375e
Максим <vokanson@gmail.com>
2022-12-07 12:03:41 +0600

    Films

--- app.js
+++ app.js
@@ -4,20 +4,28 @@ const App = {
             placeholderString: "Введите название фильма",
             title: "Поиск фильма по названию",
             inputValue: "",
+            film: ["terminator"],
 
         }
     },
     methods: {
         inputChangeHandler (event) {
             this.inputValue = event.target.value
-            console.log (inputValue)
+            // let film = event.target.value
         },
-        addNewFilm() {
-            console.log (this.inputValue)
-            alert(this.inputValue)
+
+        async addNewFilm() {
+            const films = this.inputValue
+            // this.film.push(this.inputValue)
+            const res = await fetch (`http://www.omdbapi.com/?apikey=922db138&t=${films}`);
+            const dat = await res.json();
+            console.log (dat)
+            // console.log (this.film)
+            console.log (res)
         }
     },
 }
+
 
 
 
