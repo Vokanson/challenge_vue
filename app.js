1a7ebb89573cbb0657163a2b691d942c10b22bbd
Максим <vokanson@gmail.com>
2022-12-07 18:11:52 +0600

    newCommit

--- app.js
+++ app.js
@@ -4,23 +4,36 @@ const App = {
             placeholderString: "Введите название фильма",
             title: "Поиск фильма по названию",
             inputValue: "",
-            film: ["terminator"],
+            nameFilm: "",
+            titleYear: "Год выхода:",
+            yearFilm: "",
+            aboutFilm: "",
+            genre : [],
 
         }
     },
     methods: {
         inputChangeHandler (event) {
             this.inputValue = event.target.value
-            // let film = event.target.value
         },
 
         async addNewFilm() {
             const films = this.inputValue
-            // this.film.push(this.inputValue)
             const res = await fetch (`http://www.omdbapi.com/?apikey=922db138&t=${films}`);
             const dat = await res.json();
+            this.nameFilm = dat.Title
+            this.yearFilm = dat.Year
+            this.aboutFilm = dat.Plot
+            const genreRetApi = dat.Genre
+            function splitString(stringToSplit, separator) {
+                const arrayOfString = stringToSplit.split(separator);
+                console.log (arrayOfString[1])
+                this.genre = arrayOfString
+            }
+            splitString(genreRetApi, ",")
+
+            console.log (genreRetApi)
             console.log (dat)
-            // console.log (this.film)
             console.log (res)
         }
     },
