be1d5913ba31e87f224a0513e7057fbb32d1ecbe
Vokanson <vokanson@gmail.com>
2022-12-11 19:42:51 +0500

    update

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 const App = {
     data() {
         return {
+            visible: false,
             placeholderString: "Введите название фильма",
             title: "Поиск фильма по названию",
             inputValue: "",
             nameFilm: "",
-            titleYear: "Год выхода:",
             yearFilm: "",
             aboutFilm: "",
             genre: [],
+            posterURL:"",
+            naFilm:""
 
         }
     },
     methods: {
         inputChangeHandler (event) {
             this.inputValue = event.target.value
+
         },
+
 
         addNewFilm: async function () {
             const films = this.inputValue
-            const res = await fetch(`http://www.omdbapi.com/?apikey=922db138&t=${films}`);
-            const dat = await res.json();
-            this.nameFilm = dat.Title
-            this.yearFilm = dat.Year
-            this.aboutFilm = dat.Plot
+            const res = await fetch(`http://www.omdbapi.com/?apikey=922db138&t=${films}`)
+             const dat = await res.json()
 
-            // const genreRetApi = dat.Genre
-
-            // function splitString() {
+            if (dat.Error === "Movie not found!") {
+                this.naFilm = "Фильм не найден!"
+            }
+            else{
+                this.nameFilm = dat.Title
+                this.yearFilm = "Год выхода: " + dat.Year
+                this.aboutFilm = dat.Plot
+                this.posterURL = dat.Poster
+                console.log (dat.Poster)
                 const arrayOfString = dat.Genre.split(",");
                 console.log(arrayOfString[1])
                 this.genre= arrayOfString
-            // }
-            // // this.genre = dat.Genre
-            //
-            // splitString()
+                console.log(dat.Genre)
+                console.log(dat)
+                console.log(res)
+                this.inputValue = ""
+            }
 
-            console.log(dat.Genre)
-            console.log(dat)
-            console.log(res)
         }
     },
 }
